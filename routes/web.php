ed0f1a7e8c1b6330962f6c40321155746ce9ce28
elwidi <laras.setyawidi@gmail.com>
2022-10-14 14:39:56 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/book', [App\Http\Controllers\BukuController::class, 'index']);
+Route::get('/book_show/{id}', [App\Http\Controllers\BukuController::class, 'show']);
